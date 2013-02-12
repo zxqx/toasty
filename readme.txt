5952a2b7f2b8a44d5783366857a5aa1bc3d59ee5
Zak Angelle <zaknotzak@gmail.com>
2013-02-12 00:44:27 -0600

    Toasty is born

---
+++ readme.txt
@@ -0,0 +1,20 @@
+-----------------
+Toasty.js
+-----------------
+
+SUMMARY:
+* A jQuery plugin that shows the Mortal Kombat toasty guy!
+
+REQUIRES:
+* jQuery
+
+HOW TO USE:
+* Add the toasty.js file, along with the audio and images, to your site's root directory.
+* Call the toasty() method on the element that will be clicked to show the toasty guy.
+
+EXAMPLE:
+<h1><a id="toasty" href="#">Guess what?</a></h1>
+
+<script>
+  $("#toasty").Toasty();
+</script>
---
+++ toasty.js
@@ -0,0 +1,20 @@
+(function($) {
+  $.fn.Toasty = function() {
+    $("<div id='toasty-guy' style='position: absolute; bottom: 0; right: -296px; width: 296px; height: 287px; background: url(images/toasty.png) no-repeat; display: none;'></div><audio id='toasty-sound' preload='auto'><source src='audio/toasty.mp3' type='audio/mp3' /><source src='audio/toasty.ogg' type='audio/ogg' /></audio>").appendTo("body");
+
+    this.click(function() {
+      $("#toasty-sound")[0].play();
+
+      $("#toasty-guy").show().animate({
+        right: "0",
+        queue: true,
+      }, 300).delay(500).animate({
+        right: "-296",
+        queue: true
+      }, 350, function() {
+        $(this).hide();
+      });
+    });
+
+  };
+})(jQuery);
